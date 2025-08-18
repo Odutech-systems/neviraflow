a00378988b762f735359e835eb2faaf5d4238694
FRANKSADWAR <billyfranks98@gmail.com>
2025-08-18 08:10:06 +0300

    checked hooks

--- neviraflow/hooks.py
+++ neviraflow/hooks.py
@@ -277,8 +277,8 @@ doc_events = {
     },
     "Work Order": {
         #"on_update": "batch_manager.update_work_order_completion.create_todo_on_completion",
-        "before_save": "batch_manager.work_order_timer.on_before_save",
-        "on_submit": "batch_manager.work_order_timer.on_submit",
+        "before_save": "neviraflow.work_order_timer.on_before_save",
+        "on_submit": "neviraflow.work_order_timer.on_submit",
     }
 }
 
