5db4115eef91ee7dfcf08890f4982f58d6e13850
FRANKSADWAR <billyfranks98@gmail.com>
2025-11-20 20:27:06 +0300

    create salary slip js file

--- neviraflow/nevira_workflow/doctype/prorated_salary_structure_assignment/prorated_salary_structure_assignment.py
+++ neviraflow/nevira_workflow/doctype/prorated_salary_structure_assignment/prorated_salary_structure_assignment.py
@@ -2,13 +2,53 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe.apps import _
+from frappe import _
 from frappe.model.document import Document
 
 
 class ProratedSalaryStructureAssignment(Document):
+
+	def on_submit(self):
+		self.get_employees_based_on_dates()
+
 	def validate(self):
 		self.set_payroll_payable_account()
+
+
+	@frappe.whitelist(allow_guest=True)
+	def get_employees_based_on_dates(self):
+		"""
+		This method will populate the employees table based on the joining date and the payroll start date
+		"""
+		if not self.start_date or not self.to_date:
+			frappe.throw("Please select both the start date and end date")
+
+		## Cleat the existing table
+		self.prorated_employee = []
+
+		employees = frappe.get_all(
+			"Employee", 
+			fields= ["name","employee_name","ctc","joining_date"],
+			filters = {
+				"date_of_joining": [">", self.start_date],
+				"date_of_joining": ["<=", self.to_date],
+				"status": "Active"
+			}
+		)
+
+		if not employees:
+			frappe.msgprint("No employees found in the selected date range")
+			return
+		
+		for emp in employees:
+			self.append("prorated_employees",{
+				"employee": emp.name,
+				"employee_name": emp.employee_name,
+				"joining_date": emp.date_of_joining,
+				"base_salary": emp.ctc or 0
+			})
+		frappe.msgprint(f"Added {len(employees)} employees to the prorated list.")
+		return True
 
 	def set_payroll_payable_account(self):
 		if not self.set_payroll_payable_account:
@@ -25,5 +65,3 @@ class ProratedSalaryStructureAssignment(Document):
 						"is_group": 0
 					}
 				)
-
-
