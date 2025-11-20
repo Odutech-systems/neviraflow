16f6c316e19408cf4da20134997591af0bf6b5ed
FRANKSADWAR <billyfranks98@gmail.com>
2025-11-20 17:36:25 +0300

    prorated salary structure assignment doctype

---
+++ neviraflow/nevira_workflow/doctype/prorated_salary_structure_assignment/prorated_salary_structure_assignment.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Victor Mandela and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Prorated Salary Structure Assignment", {
+// 	refresh(frm) {
+
+// 	},
+// });
