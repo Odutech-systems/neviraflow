4e9009854ddd2ac23700feb50f16fad522d795c8
FRANKSADWAR <billyfranks98@gmail.com>
2025-11-20 18:02:29 +0300

    add validation for payroll payable account

--- neviraflow/nevira_workflow/doctype/prorated_salary_structure_assignment/prorated_salary_structure_assignment.py
+++ neviraflow/nevira_workflow/doctype/prorated_salary_structure_assignment/prorated_salary_structure_assignment.py
@@ -1,9 +1,28 @@
-# Copyright (c) 2025, Victor Mandela and contributors
+# Copyright (c) 2025, Billy Franklin Adwar and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ProratedSalaryStructureAssignment(Document):
-	pass
+	def validate(self):
+		self.set_payroll_payable_account()
+
+	def set_payroll_payable_account(self):
+		if not self.set_payroll_payable_account:
+			payroll_payable_account = frappe.db.get_value(
+				"Company", self.company, "default_payroll_payable_account"
+			)
+			if not payroll_payable_account:
+				payroll_payable_account = frappe.db.get_value(
+					"Account", 
+					{
+						"account_name": _("Payroll Payable"), 
+						"company": self.company, 
+						"account_currency": frappe.db.get_value("Company", self.company, "default_currency"),
+						"is_group": 0
+					}
+				)
+
+
