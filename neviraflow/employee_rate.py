1a7e2828281539a16ff07468d85b3df5f3d71fa2
FRANKSADWAR <billyfranks98@gmail.com>
2025-11-23 12:02:57 +0300

    custom daily rate

--- neviraflow/employee_rate.py
+++ neviraflow/employee_rate.py
@@ -6,10 +6,10 @@ def set_daily_rate(doc, method=None):
     Compute the employee's daily salary amount based on the employee's 
     CTC. This is to be done before saving the employee docType
     """
-    if doc.ctc and (flt.ctc) > 0:
-        doc.daily_salary_rate = doc.ctc / 30
+    if doc.ctc and flt(doc.ctc) > 0:
+        doc.custom_daily_salary_rate = doc.ctc / 30
     else:
-        doc.ctc = 0
+        doc.custom_daily_salary_rate = 0
 
 def validate_employee_ctc(doc, method=None):
     """
