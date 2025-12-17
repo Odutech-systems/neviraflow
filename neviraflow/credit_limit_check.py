aa4e3c23f5d9d836890b4a93eccf17b0c6f1dff0
FRANKSADWAR <billyfranks98@gmail.com>
2025-12-17 11:58:30 +0000

    credit limit message

--- neviraflow/credit_limit_check.py
+++ neviraflow/credit_limit_check.py
@@ -7,8 +7,65 @@ class CreditLimitExceedError(frappe.ValidationError):
     pass
 
 
-def validate_credit_limit(doc):
-    pass
+def validate_credit_limit(doc, method=None):
+    if not doc.customer:
+        return
+    
+    customer_credit_limit = get_customer_credit_limit(doc.customer)
+    
+    if not customer_credit_limit or 0:
+        return
+    total_outstanding = get_customer_outstanding_amount(doc.customer)
+
+    current_order_amount = get_sales_order_amount(doc)
+
+    ## Get the projected total after order is created
+    projected_total = total_outstanding + current_order_amount
+
+    ## Current available credit
+    current_available_credit = customer_credit_limit - total_outstanding
+    
+    credit_utilization_percentage = (total_outstanding / customer_credit_limit * 100) if customer_credit_limit > 0 else 0
+
+    if projected_total > customer_credit_limit:
+        frappe.throw(
+            _("Credit limit Exceeded for customer : {0}<br>br>"
+              "Credit Summary: </br></br>"
+              "Credit Limit: {1} </br>"
+              "Current Outstanding amount: {2} ({3}% Utilised) </br>"
+              "This order amount : {4} </br>"
+              "Projected Total: {5} </br>"
+              "Available credit: {6} </br></br>"
+              "Credit exceeded by: {7} </br>"
+              "Possible Solutions: <br>"
+              "1. Request credit limit increase or reduce the order amount"
+            ).format(
+                frappe.bold(doc.customer_name),
+                frappe.utils.fmt_money(customer_credit_limit),
+                frappe.utils.fmt_money(total_outstanding),
+                round(credit_utilization_percentage,2),
+                frappe.utils.fmt_money(current_order_amount),
+                frappe.utils.fmt_money(projected_total),
+                frappe.utils.fmt_money(current_available_credit),
+                frappe.utils.fmt_monet(projected_total - customer_credit_limit)
+            ), title=_("Credit Limit Exceeded"), 
+            exc=CreditLimitExceedError,
+        )
+        
+    elif credit_utilization_percentage >= 60:
+        frappe.msgprint(
+            _("High Credit Utilization for {0} </br>"
+              "Credit Limit: {1} </br>"
+              "Current Utilization: {2}% <br>"
+            ).format(
+                  doc.customer_name,
+                  frappe.utils.fmt_money(customer_credit_limit),
+                  round(credit_utilization_percentage,2)
+            ),
+            title=_("High credit Utilization warning"),
+            indicator = "yellow"
+        )
+
 
 
 def get_customer_outstanding_amount(customer):
