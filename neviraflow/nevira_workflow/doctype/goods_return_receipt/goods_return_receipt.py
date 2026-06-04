5eb37d86217ecd0b5f1a1f571378f9ef35c7dcef
FRANKSADWAR <billyfranks98@gmail.com>
2026-06-04 08:50:37 +0000

    goods returned receipts

---
+++ neviraflow/nevira_workflow/doctype/goods_return_receipt/goods_return_receipt.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2026, Victor Mandela, Billy Adwar & Moses Njue and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+from frappe.utils import getdate
+
+## Have a button action that can be triggered at any time to send an email to users, as an API 
+## On click of the button, an email is sent
+
+
+class GoodsReturnReceipt(Document):
+	def before_save(self):
+		## Set details on the document before it is saved
+		customer, customer_name = get_customer_name(self)
+		self.customer = customer
+		self.customer_name = customer_name
+
+	def after_submit(self):
+		pass
+
+	def get_customer_name(self):
+		## Extract some details and return values
+		dnote = self.delivery_note
+		customer_id = frappe.db.get_value('Delivery Note',dnote,'customer')
+		customer_name = frappe.db.get_value('Delivery Note', dnote,'customer_name')
+		return customer_id, customer_name
+
+	def send_mail_after_submit():
+		## Send an email to users after submit
+		pass
