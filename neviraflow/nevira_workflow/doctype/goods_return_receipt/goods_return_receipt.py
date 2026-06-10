04800f4dd8d1c203dd9b5937a92c477e0120dfec
FRANKSADWAR <billyfranks98@gmail.com>
2026-06-10 11:39:50 +0000

    added send mail functionality

--- neviraflow/nevira_workflow/doctype/goods_return_receipt/goods_return_receipt.py
+++ neviraflow/nevira_workflow/doctype/goods_return_receipt/goods_return_receipt.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2026, Victor Mandela, Billy Adwar & Moses Njue and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 from frappe.utils import getdate
 
@@ -12,12 +12,12 @@ from frappe.utils import getdate
 class GoodsReturnReceipt(Document):
 	def before_save(self):
 		## Set details on the document before it is saved
-		customer, customer_name = get_customer_name(self)
+		customer, customer_name = self.get_customer_name(self)
 		self.customer = customer
 		self.customer_name = customer_name
 
 	def after_submit(self):
-		pass
+		self.send_mail_after_submit()
 
 	def get_customer_name(self):
 		## Extract some details and return values
@@ -26,6 +26,25 @@ class GoodsReturnReceipt(Document):
 		customer_name = frappe.db.get_value('Delivery Note', dnote,'customer_name')
 		return customer_id, customer_name
 
-	def send_mail_after_submit():
+	def send_mail_after_submit(self):
 		## Send an email to users after submit
-		pass
+		mail_list = ["billy.franks@neviraminerals.com",
+			   		"sales@neviraminerals.com",
+					"fiona@neviraminerals.com",
+			   		"jackline.akinyi@neviraminerals.com",
+					"steve@neviraminerals.com"]
+					
+		subject = "ALERT: Potential Credit Note or Inventory Check due to return"
+		sender = "systems@neviraminerals.com"
+
+		frappe.sendmail(
+			recipients = mail_list,
+			sender = sender,
+			subject = subject,
+			message = f"Please check the return for delivery note {self.delivery_note}.\nThere is a possible need to create a credit note or material receipt.",
+			reference_doctype = self.doctype,
+			reference_name = self.name
+
+		)
+		frappe.db.commit()
+		frappe.msgprint("Alert message sent!")
